8a11998cbc2d27182d43d69298b866a33f5d2ff8
Pepe <piotrpustul1@gmail.com>
2023-09-06 16:38:36 +0100

    started work on the backend, installed express, bodyparser and nodemon, created routes structure for places and users, added first middlewares

---
+++ backend/routes/users-routes.js
@@ -0,0 +1,12 @@
+const express = require('express')
+
+const router = express.Router()
+
+const DUMMY_USERS = [
+  {
+    id: 'u1',
+    name: 'Piotr',
+    image: 'https://picsum.photos/id/237/536/354',
+    places: 3,
+  },
+]
