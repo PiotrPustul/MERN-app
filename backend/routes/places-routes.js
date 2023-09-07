93407db3fb4e39cfcb004eaba466c198af31987f
Pepe <piotrpustul1@gmail.com>
2023-09-07 23:53:37 +0100

    added models, implemented a new HttpError model

--- backend/routes/places-routes.js
+++ backend/routes/places-routes.js
@@ -1,5 +1,7 @@
 const express = require('express')
 
+const HttpError = require('../models/http-error')
+
 const router = express.Router()
 
 const DUMMY_PLACES = [
@@ -32,6 +34,10 @@ router.get('/:pid', (req, res, next) => {
 
   const place = DUMMY_PLACES.find((p) => p.id === placeId)
 
+  if (!place) {
+    throw new HttpError('Could not find the place for the provided id', 404)
+  }
+
   res.json({ place })
 })
 
@@ -39,6 +45,12 @@ router.get('/user/:uid', (req, res, next) => {
   const userId = req.params.uid
 
   const place = DUMMY_PLACES.find((p) => p.creator === userId)
+
+  if (!place) {
+    return next(
+      new HttpError('Could not find the user for the provided id', 404)
+    )
+  }
 
   res.json({ place })
 })
