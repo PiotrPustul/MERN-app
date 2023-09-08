ac8179121be74def4335448713c75cba0a65d1c1
Pepe <piotrpustul1@gmail.com>
2023-09-08 13:54:32 +0100

    created a post route, added create place controller, installed a new npm uuid to create a unique id for a new place

--- backend/controllers/places-controllers.js
+++ backend/controllers/places-controllers.js
@@ -1,3 +1,4 @@
+const { v4: uuidv4 } = require('uuid')
 const HttpError = require('../models/http-error')
 
 const DUMMY_PLACES = [
@@ -51,5 +52,25 @@ const getPlaceByUserId = (req, res, next) => {
   res.json({ place })
 }
 
+const createPlace = (req, res, next) => {
+  const { title, description, coordinates, address, creator } = req.body
+
+  const createdPlace = {
+    id: uuidv4(),
+    title,
+    description,
+    location: coordinates,
+    address,
+    creator,
+  }
+
+  DUMMY_PLACES.push(createdPlace)
+
+  console.log(DUMMY_PLACES)
+
+  res.status(201).json({ place: createdPlace })
+}
+
 exports.getPlaceById = getPlaceById
 exports.getPlaceByUserId = getPlaceByUserId
+exports.createPlace = createPlace
