e949c9ce8f800bbe523f2e13352f99aa365d66cc
Pepe <piotrpustul1@gmail.com>
2023-09-14 19:58:45 +0100

    installed mongoose and dotenv packages, established a connection with a mongoDB

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,16 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
 
 const placesRoutes = require('./routes/places-routes')
 const usersRoutes = require('./routes/users-routes')
 const HttpError = require('./models/http-error')
+
+dotenv.config()
+const mongoDB_User = process.env.MDB_USER
+const mongoDB_Pass = process.env.MDB_PASSWORD
+const port = process.env.PORT
 
 const app = express()
 
@@ -24,4 +31,13 @@ app.use((error, req, res, next) => {
   res.json({ message: error.message || 'An unknown error occured' })
 })
 
-app.listen(8000)
+mongoose
+  .connect(
+    `mongodb+srv://${mongoDB_User}:${mongoDB_Pass}@mern-app.easrui5.mongodb.net/places?retryWrites=true&w=majority`
+  )
+  .then(() => {
+    app.listen(port)
+  })
+  .catch((error) => {
+    console.log(error)
+  })
