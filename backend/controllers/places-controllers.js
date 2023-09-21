dbd4fae6616c5ca811f4adaa964abe445d7bb21c
Pepe <piotrpustul1@gmail.com>
2023-09-21 12:58:25 +0100

    worked on the getPlacesByUserId, adding populate method

--- backend/controllers/places-controllers.js
+++ backend/controllers/places-controllers.js
@@ -31,9 +31,9 @@ const getPlaceById = async (req, res, next) => {
 const getPlacesByUserId = async (req, res, next) => {
   const userId = req.params.uid
 
-  let places
+  let userWithPlaces
   try {
-    places = await Place.find({ creator: userId })
+    userWithPlaces = await User.findById(userId).populate('places')
   } catch (err) {
     const error = new HttpError(
       'Could not find a user places, please try again later',
@@ -42,13 +42,15 @@ const getPlacesByUserId = async (req, res, next) => {
     return next(error)
   }
 
-  if (!places || places.length === 0) {
+  if (!userWithPlaces || userWithPlaces.length === 0) {
     return next(
       new HttpError('Could not find places for the provided user ID', 404)
     )
   }
 
-  res.json({ places: places.map((place) => place.toObject({ getters: true })) })
+  res.json({
+    places: userWithPlaces.map((place) => place.toObject({ getters: true })),
+  })
 }
 
 const createPlace = async (req, res, next) => {
