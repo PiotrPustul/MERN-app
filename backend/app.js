5ce36402296bba59b85499c1f10a681507716bec
Pepe <piotrpustul1@gmail.com>
2023-09-23 16:04:11 +0100

    worked on the /uploads/images middleware to return stored image on the frontend

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,5 @@
 const fs = require('fs')
+const path = require('path')
 
 const express = require('express')
 const bodyParser = require('body-parser')
@@ -12,6 +13,12 @@ const HttpError = require('./models/http-error')
 const app = express()
 
 app.use(bodyParser.json())
+
+/**
+ * Handle all req starting with the path /upload/images
+ * express.static will return file stored in /upload/images
+ */
+app.use('/uploads/images', express.static(path.join('uploads', 'images')))
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*')
