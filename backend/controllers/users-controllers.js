6db20784ca06995e694391892009e2fb988ac9f1
Pepe <piotrpustul1@gmail.com>
2023-09-26 15:11:24 +0100

    generated token on backend then login and signup

--- backend/controllers/users-controllers.js
+++ backend/controllers/users-controllers.js
@@ -1,5 +1,6 @@
 const { validationResult } = require('express-validator')
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
 const HttpError = require('../models/http-error')
 const User = require('../models/userSchema')
@@ -81,7 +82,24 @@ const signup = async (req, res, next) => {
     return next(error)
   }
 
-  res.status(201).json({ user: newUser.toObject({ getters: true }) })
+  let token
+  try {
+    token = jwt.sign(
+      { userId: newUser.id, email: newUser.email },
+      'supersecret_dont_share',
+      { expiresIn: '1h' }
+    )
+  } catch (err) {
+    const error = new HttpError(
+      'Signing up failed, please try again later.',
+      500
+    )
+    return next(error)
+  }
+
+  res
+    .status(201)
+    .json({ userId: newUser.id, email: newUser.email, token: token })
 }
 
 const login = async (req, res, next) => {
@@ -120,9 +138,25 @@ const login = async (req, res, next) => {
     return next(error)
   }
 
+  let token
+  try {
+    token = jwt.sign(
+      { userId: existingUser.id, email: existingUser.email },
+      'supersecret_dont_share',
+      { expiresIn: '1h' }
+    )
+  } catch (err) {
+    const error = new HttpError(
+      'Logging in failed, please try again later.',
+      500
+    )
+    return next(error)
+  }
+
   res.status(200).json({
-    message: 'Logged in',
-    user: existingUser.toObject({ getters: true }),
+    userId: existingUser.id,
+    email: existingUser.email,
+    token: token,
   })
 }
 
